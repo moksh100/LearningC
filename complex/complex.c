f4b3dbf6f089058299dbb1660ef1104734ce0f76
moksh100 <moksh100@mail.chapman.edu>
2015-10-27 14:22:26 -0700

    complex mod

--- complex/complex.c
+++ complex/complex.c
@@ -5,19 +5,47 @@ struct ComplexNumber{
 	int y;
 };
 
-ComplexNumber mult2(ComlexNumber a, ComplexNumber b){
+struct ComplexNumber mult2(struct ComplexNumber a,struct ComplexNumber b);
+struct ComplexNumber square(struct ComplexNumber a);
+struct ComplexNumber add2(struct ComplexNumber a, struct ComplexNumber b);
+struct ComplexNumber juliamap(struct ComplexNumber z, struct ComplexNumber c);
+char* complex_to_str(struct ComplexNumber z); 
+
+int main(void){
+	return 0;
+}
+
+char* complex_to_str(struct ComplexNumber z){
+	char tagstr[160];
+	sprintf(tagstr, "%d+%di", z.x, z.y);
+	return tagstr;
+}
+
+struct ComplexNumber juliamap(struct ComplexNumber z, struct ComplexNumber c){
 	struct ComplexNumber result;
-	result.x= ((a.x*b.x)-(a.y*b.y));
-	result.y= ((a.x*b.y)+(b.x*a.y));
+	result = add2(square(z),c);
 	return result;
 }
 
-int square (ComplexNumber a){
+struct ComplexNumber add2(struct ComplexNumber a, struct ComplexNumber b){
 	struct ComplexNumber result;
-	result.x=(a.x*a.x)-(a.y*a.y);
-	result.y=a.y*a.y; 
+	result.x=a.x+b.x;
+	result.y=a.y+b.y;
+	return result;
 }
 
-int main(void) {
-	
-}
+
+
+struct ComplexNumber mult2(struct ComplexNumber a, struct ComplexNumber b){
+	struct ComplexNumber result;
+	result.x=((a.x*b.x)-(a.y*b.y));
+	result.y=((a.x*b.y)+(b.x*a.y));
+	return result;
+};
+
+struct ComplexNumber square (struct ComplexNumber a){
+	struct ComplexNumber result;
+	result.x=(a.x*a.x)-(a.y*a.y);
+	result.y=a.y*a.y;
+	return result; 
+};
