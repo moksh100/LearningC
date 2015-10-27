2307134addc9983ae29be95bea8cfb7f663e202e
moksh100 <moksh100@mail.chapman.edu>
2015-10-27 13:38:26 -0700

    change to long fibonacci

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int main(void){
-	int initF1=1;
-	int initF2=1;
+	long initF1=1;
+	long initF2=1;
 	printf("%d,%d",initF1, initF2);
 	int i;
+	long r;
 	for (i=0; i<100; i++){
-		int r=initF1+initF2;
+		r=initF1+initF2;
 		if(r<0){
 			printf("\nbreak on %d, int out of range\n", i);
 			break;
 		}
-		printf(",%d",r);
+		printf(",%lu",r);
 		initF1=initF2;
 		initF2=r;
 		
