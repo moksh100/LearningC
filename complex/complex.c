2f048d9370de2ed7ea09abe627664b68ffc826b4
moksh100 <moksh100@mail.chapman.edu>
2015-11-17 19:12:19 -0800

    add header

--- complex/complex.c
+++ complex/complex.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
+#include "complex.h"
 
-struct ComplexNumber{
-	int x;
-	int y;
-};
-
-struct ComplexNumber mult2(struct ComplexNumber a,struct ComplexNumber b);
-struct ComplexNumber square(struct ComplexNumber a);
-struct ComplexNumber add2(struct ComplexNumber a, struct ComplexNumber b);
-struct ComplexNumber juliamap(struct ComplexNumber z, struct ComplexNumber c);
-char ** complex_to_str(struct ComplexNumber z); 
-void test(void);
 
 int main(void){
 	test();
